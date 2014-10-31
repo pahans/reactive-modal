69e0da0473f9a3a776e16356e1f6824882027fd7
wathsala3000 <wathsala3000@gmail.com>
2014-10-31 16:25:34 +0530

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Reactive bootstrap modals for meteor",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/pahans/reactive-modal.git",
   name: "pahans:reactive-modal"
 });
